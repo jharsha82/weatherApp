105cd440b8a894bbe9f743f8734f4112af963561
Harsha Jain <jharsha82@gmail.com>
2021-06-26 14:11:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let weather = {
+    api_keys: '4489913fcdd59295052d44618f9951b6',
+    fetchWeather: function(city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='
+        + city  
+        +'&units=metric&appid=' 
+        + this.api_keys
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { description, icon } = data.weather[0];
+        const { speed } = data.wind;
+        const { temp, humidity  } = data.main; 
+
+        document.querySelector('.city').innerText = 'Weather in' + ' ' + name;
+        document.querySelector('.temp').innerText =  temp + '°C';
+        document.querySelector('.description').innerText =  description;
+        document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + ' ' + '%';
+        document.querySelector('.wind').innerText = 'Wind: ' + speed + ' ' + 'km/h';
+        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '.png';
+        document.querySelector('.weather').classList.remove('loading');
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector('.search-bar').value);
+    }
+};
+
+
+document.querySelector('.search-button').addEventListener('click', function() {
+    weather.search();
+});
+
+document.querySelector('.search-bar').addEventListener('keyup', function (event) {
+    if (event.key == 'Enter') {
+        weather.search();
+    }
+})
+
+weather.fetchWeather('Denver');
